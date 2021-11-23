f94fe93a00e330795a7e2d44481da02d3a198f3b
Andreea Chelaru <andreea.chelaru@aula.education>
2021-11-23 18:59:59 +0200

    add tailwind css

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,10 +3,10 @@ import Layout from '../components/Layout'
 
 const IndexPage = () => (
   <Layout title="Home | Next.js + TypeScript Example">
-    <h1>Hello Next.js 👋</h1>
-    <p>
+    <h1 className="header">Hello Next.js 👋</h1>
+    <p className="text-center p-6 mt-4 mx-auto bg-blue-200">
       <Link href="/about">
-        <a>About</a>
+        <a className="text-blue-600 hover:text-white">About</a>
       </Link>
     </p>
   </Layout>
